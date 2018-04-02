05989071fc950f8f6cccbfc732ec23f1821ac7e6
Matt Bernhardt <mjbernha@mit.edu>
2018-04-02 11:56:06 -0400

    Moves site settings into a common fieldset for easier organization

--- theme-settings.php
+++ theme-settings.php
@@ -9,7 +9,19 @@
  * Adds background image control to theme settings.
  */
 function ideabank_form_system_theme_settings_alter(&$form, $form_state) {
-  $form['background_image'] = array(
+
+  // This puts all defined controls in a single fieldset, to match the way
+  // other controls are grouped.
+  $form['ideabank'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Ideabank settings'),
+    '#description' => t('These options will control how various aspects of the site will be (or will not be) displayed.'),
+    '#access' => user_access('administer site configuration') && user_access('access administration pages'),
+  );
+
+  // This enables the site builder to choose which background image is
+  // displayed on all pages.
+  $form['ideabank']['background_image'] = array(
     '#type'          => 'select',
     '#title'         => t('Background'),
     '#options'       => array(
@@ -21,14 +33,18 @@ function ideabank_form_system_theme_settings_alter(&$form, $form_state) {
     '#description'   => t("What image should be used as a background? Choosing 'None' will result in a gray gradient being displayed."),
   );
 
-  $form['libraries_logo'] = array(
+  // This determines whether the MIT Libaries' logo is displayed in the site
+  // masthead.
+  $form['ideabank']['libraries_logo'] = array(
     '#type'          => 'checkbox',
     '#title'         => t('Show MIT Libraries Logo'),
     '#default_value' => theme_get_setting('libraries_logo'),
     '#description'   => t("Should the MIT Libraries' logo be displayed at the top of the site?"),
   );
 
-  $form['search_header'] = array(
+  // This determines whether the search form (provided by a Google search
+  // appliance) is loaded in the site header.
+  $form['ideabank']['search_header'] = array(
     '#type'          => 'checkbox',
     '#title'         => t('Show search in header'),
     '#default_value' => theme_get_setting('search_header'),
